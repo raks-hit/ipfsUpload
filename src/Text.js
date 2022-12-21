84ba4a4496c168f87b8be172bcd2669361c6020f
Rakshit <coderakshit@gmail.com>
2022-12-21 15:44:34 +0530

    first

--- src/Text.js
+++ src/Text.js
@@ -1,9 +1,10 @@
+import React from 'react'
 import { useState } from 'react';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
 import Loader from './Loader';
 import { encode as base64_encode } from 'base-64';
-import { Text } from './Text';
+
 // require('dotenv').config()
 const IPFS = require('ipfs-api');
 // import {create as IPFS} from 'ipfs-http-client'
@@ -16,28 +17,34 @@ const ipfs = new IPFS({
     Authorization: 'Basic ' + encodedSecrets
   }
 });
-
-function App() {
-  const [buf, setBuf] = useState();
+export const Text = () => {
+    const [buf, setBuf] = useState();
   const [hash, setHash] = useState("");
   const [loader, setLoader] = useState(false);
   const [showLinks, setShowLinks] = useState(false);
 
+  const [text,setText]=useState({});
+
   const captureFile = (event) => {
     event.stopPropagation()
     event.preventDefault()
-    const file = event.target.files[0]
+    const text = event.target.value
     // const file=event.target.value;
-    let reader = new window.FileReader()
-    reader.readAsArrayBuffer(file)
-    reader.onloadend = () => convertToBuffer(reader)
-    // convertToBuffer(file);
+    // let reader = new window.FileReader()
+    // reader.readAsArrayBuffer(file)
+    // reader.onloadend = () => convertToBuffer(reader)
+    convertToBuffer(text);
   };
-
+const showText=async()=>{
+    let result=await fetch("https://ipfs.io/ipfs/QmYS2AUJGyxGCAvmvthqdj4vwVm7paHNB82aEYY6tdtf5j");
+    let data=result.toString();
+    setText(data);
+}
   const convertToBuffer = async (reader) => {
     //file is converted to a buffer to prepare for uploading to IPFS
-    const buffer = await Buffer.from(reader.result);
+    const buffer = await Buffer.from(reader);
     setBuf(buffer);
+    // setText(buffer.toString());
   };
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -53,8 +60,16 @@ function App() {
         console.error(err)
         alert('An error occurred. Please check the console');
       })
-    if (ipfsId)
+    if (ipfsId){
+       await showText();
       setShowLinks(true)
+     
+      
+      
+
+    }
+     
+
     else
       setShowLinks(false)
     setLoader(false);
@@ -66,27 +81,28 @@ function App() {
   }
   return (
     <div>
-      <h1>Upload files to IPFS</h1>
-      <h5> Choose file to upload to IPFS </h5>
-      <Form onSubmit={onSubmit}>
-        <input type="file" onChange={captureFile} required />
-        <Button type="submit">Upload</Button>
-      </Form>
-      {
-        showLinks ?
-          <div>
-            <p>---------------------------------------------------------------------------------------------</p>
-            <h6>IPFS Hash: {hash}</h6>
-            <p>Non clickabe Link: https://ipfs.io/ipfs/{hash}</p>
-            <a href={"https://ipfs.io/ipfs/" + hash}>Clickable Link to view file on IPFS</a>
-            <img src={"https://ipfs.io/ipfs/" + hash} alt=''/>
-          </div> :
-          <p></p>
+    <h1>Upload files to IPFS</h1>
+    <h5> Choose file to upload to IPFS </h5>
+    <Form onSubmit={onSubmit}>
+      <input type="textarea" onChange={captureFile} required />
+      <Button type="submit">Upload</Button>
+    </Form>
+    {
+      showLinks ?
+        <div>
+          <p>---------------------------------------------------------------------------------------------</p>
+          <h6>IPFS Hash: {hash}</h6>
+          <p>Non clickabe Link: https://ipfs.io/ipfs/{hash}</p>
+          <a href={"https://ipfs.io/ipfs/" + hash}>Clickable Link to view file on IPFS</a>
+          <img src={"https://ipfs.io/ipfs/" + hash} alt=''/>
+          <div>{text}</div>
+          
+          
+        </div> :
+        <p></p>
 
-      }
-      <Text/>
-    </div>
-  );
+    }
+    
+  </div>
+  )
 }
-
-export default App;
